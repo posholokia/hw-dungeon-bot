f37cdf21c5b1ab98a8f9f4a3f6cca13ba9632cd4
Илья Пошолок <ilya.posholokk@gmail.com>
2026-08-13 19:40:00 +0600

    багфикс

--- src/output/mouse/mouse.py
+++ src/output/mouse/mouse.py
@@ -5,14 +5,25 @@ from logging import Logger
 from pynput.mouse import Button, Controller  # type: ignore [import-untyped]
 from structlog import getLogger
 
+from domain.types import Coordinate, no_value
+from interfaces.output import IMouseClick
+
 logger: Logger = getLogger(__name__)
 
 
-class MouseController:
+class MouseController(IMouseClick):
     def __init__(self) -> None:
         self._mouse = Controller()
 
-    def click(self, x: int, y: int) -> None:
+    def mouse_click(
+        self, c: Coordinate, width: int = no_value, height: int = no_value
+    ) -> None:
+        if width and height:
+            x = random.randint(c[0], c[0] + width - 1)
+            y = random.randint(c[1], c[1] + height - 1)
+        else:
+            x = c[0]
+            y = c[1]
         time.sleep(random.uniform(0.086, 0.134))
         self._mouse.position = (x, y)
         self._mouse.click(Button.left)
